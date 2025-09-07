e04f69eb42ca940b492f5cc6c1c83468b37dfbb9
Akshaj Dev <akshajdevkv@gmail.com>
2025-09-07 18:22:19 +0530

    add user input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import logo from "../assets/investment-calculator-logo.png";
 
 export default function Header(){
-    return <header>
-        <img src={logo} />
+    return <header id="header">
+        <img src={logo} alt="money bag logo"/>
         <h1>Investment Calculator</h1>
     </header>
 }
