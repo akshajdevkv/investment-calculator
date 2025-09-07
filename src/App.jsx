860ec13bcf8ade74eed53a6bde934595b3a3d304
Akshaj Dev <akshajdevkv@gmail.com>
2025-09-07 18:57:29 +0530

    lift state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import Header from "./components/Header"
+import Results from "./components/Results";
 import UserInput from "./components/UserInput"
+import {useState} from "react";
 export default function App(){
+   const [userInput,setUserInput]= useState({
+        initialInvestment:1000,
+        annualInvestment:1200,
+        expectedReturn:6,
+        duration:10,
+    })
+
+    function handleChange(inputIdentifier,newValue){
+        setUserInput(prevUserInput=>{
+            return {
+                ...prevUserInput,
+                [inputIdentifier]:newValue,
+            }
+        })
+    }
   return  <>
     <Header/>
-    <UserInput/>
+  <UserInput userInput={userInput}  onChange={handleChange}/>
+  <Results input={userInput}/>
   </>
 }
