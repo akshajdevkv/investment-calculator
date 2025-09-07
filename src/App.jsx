8bc52566eaf14b6594964ffdf230363f71bc7e13
Akshaj Dev <akshajdevkv@gmail.com>
2025-09-07 18:40:12 +0530

    implement user input logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Header from "./components/Header"
+import UserInput from "./components/UserInput"
 export default function App(){
   return  <>
     <h1>Hello world</h1>
     <Header/>
+    <UserInput/>
   </>
 }
